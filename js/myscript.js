602e015e4e2124c8769bdd7e76a8778f71bebace
M Bayu Misbahudin <mbm.16616@gmail.com>
2020-03-26 22:48:56 +0700

    add parallax effect

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,5 @@
-$(document).ready(function(){
-	$('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width:$(this).attr('data-percent')
-		},6000);
-	});
-
-	$('.smooth-scrl').on('click',function(e){
+//smooth scroll
+$('.smooth-scrl').on('click',function(e){
 
 		let link = $(this).attr('href');
 		let dest = $(link);
@@ -13,5 +7,36 @@ $(document).ready(function(){
 			scrollTop: $(dest).offset().top-50
 		},1500);
 	});
-});
 
+//parallax effect
+$(window).scroll(function(){
+	let scrl = $(this).scrollTop();
+
+	$('.header img').css({
+		'transform':`translate(-${scrl / 8}%, 0px )`
+	})
+
+	$('.header .judul').css({
+		'transform':`translate(${scrl / 8}%, 0px )`
+	})
+
+	if(scrl>$('.skills').offset().top-400){
+		$('.skillbar').each(function(){
+			$(this).find('.skillbar-bar').animate({
+				width:$(this).attr('data-percent')
+			},4500);
+		});
+	}
+
+	if(scrl>$('.socmed').offset().top-400){
+		$('.socmed .icn').each(function(i){
+			setTimeout(function(){
+				$('.socmed .icn').eq(i).addClass('landing');
+			},375*(i+1));
+		});
+	}
+
+	if(scrl>$('.socmed').offset().top-250){
+		$('.socmed .judul').addClass('landing');
+	}
+});
